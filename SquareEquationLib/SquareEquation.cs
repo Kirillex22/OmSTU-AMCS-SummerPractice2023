d4f95f4b86ee89570fb5039c2af67d7cd20795b3
Kirill <ziabrew34@gmail.com>
2023-06-24 05:58:17 +0000

    OMSTU

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -4,7 +4,35 @@ public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        throw new NotImplementedException();
-        Console.WriteLine("");
+        double Epsilon = 1e-5;
+        if (Math.Abs(a)<Epsilon)
+            throw new System.ArgumentException();
+        else if (double.IsNaN(a)||(double.IsPositiveInfinity(a))||(double.IsNegativeInfinity(a)))
+            throw new ArgumentException();
+        else if (double.IsNaN(b)||(double.IsPositiveInfinity(b))||(double.IsNegativeInfinity(b)))
+            throw new ArgumentException();
+        else if (double.IsNaN(c)||(double.IsPositiveInfinity(c))||(double.IsNegativeInfinity(c)))
+            throw new ArgumentException();
+
+        double D = Math.Pow(b,2)-4*a*c;
+
+        if (D >= Epsilon){
+            var roots1 = new double[2];
+            double x1 = -(b + Math.Sign(b)*Math.Sqrt(D))/2;
+            double x2 = c/x1;
+            roots1[0] = x1;
+            roots1[1] = x2;
+            return roots1;
+        }
+        else if (Math.Abs(D) < Epsilon){
+            var roots2 = new double[1];
+            double x1 = -(b + Math.Sign(b)*Math.Sqrt(D))/2;
+            roots2[0] = x1;
+            return roots2;
+        }
+        else{
+            var roots3 = new double[]{};
+            return roots3;
+        }
     }
 }
